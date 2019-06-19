5b16681441f8781a738a9a8fce81df27b66bdb3a
Mario Jamid <coding.gimp@gmail.com>
2019-06-19 13:23:58 -0700

    Simple madlib game in Python.

---
+++ python_madlib_game/madlib.py
@@ -0,0 +1,9 @@
+verb = input('Please enter a verb:\n')
+noun = input('Please enter a noun:\n')
+adj = input('Please enter an adjective:\n')
+
+print('I enjoy practice! I find it helps me to ' + verb + ' better.')
+print('Without practice, my ' + noun + ' would probably not even work.')
+print('My code is getting more ' + adj + ' every single day!')
+
+
