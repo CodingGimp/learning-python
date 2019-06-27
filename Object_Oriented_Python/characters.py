f022052c0af175d28ca146016b1be0ac944b700e
Mario Jamid <coding.gimp@gmail.com>
2019-06-27 14:28:38 -0700

    Method Arguments

--- Object_Oriented_Python/characters.py
+++ Object_Oriented_Python/characters.py
@@ -3,11 +3,22 @@ import random
 class Thief:
     sneaky = True
 
+    def __init__(self, name, sneaky=True, **kwargs):
+        self.name = name
+        self.sneaky = sneaky
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
     def pickpocket(self):
-        if self.sneaky:
-            return bool(random.randint(0, 1))
-        return False
+        return self.sneaky and bool(random.randint(0, 1))
 
-Gimp = Thief()       
+    def hide(self, light_level):
+        return self.sneaky and light_level < 10
+        
 
-print(Gimp.pickpocket())
+Gimp = Thief('Omair', scars=None, favorite_weapon='dagger')       
+print(Gimp.name)
+print(Gimp.sneaky)
+print(Gimp.favorite_weapon)
